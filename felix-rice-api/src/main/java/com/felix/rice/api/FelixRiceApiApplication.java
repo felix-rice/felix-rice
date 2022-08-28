7b36be6fba928a8993f298bf040801205f833f66
lixin40 <lixin40@kuaishou.com>
2022-08-29 00:33:18 +0800

    + 初始化spring-boot项目，用于后续开发学习中间件与spring-boot特性

---
+++ felix-rice-api/src/main/java/com/felix/rice/api/FelixRiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.felix.rice.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FelixRiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FelixRiceApiApplication.class, args);
+    }
+
+}
---
+++ felix-rice-runner/src/main/java/com/felix/rice/runner/FelixRiceRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.felix.rice.runner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FelixRiceRunnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FelixRiceRunnerApplication.class, args);
+    }
+
+}
